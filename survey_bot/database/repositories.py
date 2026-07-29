aab6982ec072cc9a1161af9ebf3a75e49bec5ae6
Kpawzzz <kpus1337@gmail.com>
2026-07-29 17:22:44 +0300

    Function update fix

--- survey_bot/database/repositories.py
+++ survey_bot/database/repositories.py
@@ -34,9 +34,16 @@ class SettingsRepository:
     async def get_settings() -> Dict[str, Any]:
         async with aiosqlite.connect(settings.DB_NAME) as db:
             db.row_factory = aiosqlite.Row
-            cursor = await db.execute("SELECT survey_link, message_text, scheduled_time FROM survey_settings WHERE id = 1")
+            # ИСПРАВЛЕНИЕ: Добавили welcome_text в список запрашиваемых полей
+            cursor = await db.execute("SELECT survey_link, message_text, welcome_text, scheduled_time FROM survey_settings WHERE id = 1")
             row = await cursor.fetchone()
             return dict(row) if row else {}
+
+    @staticmethod
+    async def update_setting(field: str, value: Union[str, datetime, None]):
+        async with aiosqlite.connect(settings.DB_NAME) as db:
+            await db.execute(f"UPDATE survey_settings SET {field} = ? WHERE id = 1", (value,))
+            await db.commit()
 
     @staticmethod
     async def update_setting(field: str, value: Union[str, datetime, None]):
